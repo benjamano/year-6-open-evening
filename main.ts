980b951e730ec106f40c51fc3236850620ae0e5d
Ben Mercer <104690692+benjamano@users.noreply.github.com>
2022-09-28 19:22:54 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-kitronik_servo_lite.driveForwards(10)
+kitronik_servo_lite.setDistancePerSecond(0.5)
+basic.showIcon(IconNames.Happy)
+let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
 basic.forever(function () {
-	
+    kitronik_servo_lite.driveForwards(100)
 })
